f091b642b2157acfc096670b0d564582400d413c
NoahAcosta <noahacosta@yahoo.com>
2021-12-01 21:05:54 -0600

    name change

--- test.java
+++ test.java
@@ -1,11 +1,12 @@
+// Noah Acosta- A simple test program
 import java.util.*;
 public class test {
     public static void main (String [] args)
     {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Please enter your name: ");
-		String nombre = in.nextLine();
-		System.out.println("Your name is: " + nombre);
+		String name = in.nextLine();
+		System.out.println("Your name is: " + name);
 		in.close();
     }
 } // END
